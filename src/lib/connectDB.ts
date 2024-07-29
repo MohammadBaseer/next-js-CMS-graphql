d3a6915ae966f9d5e2147ae74af87280738c48dc
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-07-29 16:10:25 +0200

    Mongoose DB Created

--- src/lib/connectDB.ts
+++ src/lib/connectDB.ts
@@ -1,29 +1,40 @@
-import mongoose from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
 import * as dotenv from "dotenv";
+import pc from "picocolors";
+
 dotenv.config();
 
-const MONGODB_URL = process.env.MONGODB_URL;
 
-const connectDB = async () => {
-  const connectionState = mongoose.connection.readyState;
-  if (connectionState === 1) {
-    console.log("Already MongoDB Connected");
-    return;
-  }
-  if (connectionState === 2) {
-    console.log("Connecting");
-    return;
-  }
+const DATABASE_URL = process.env.DATABASE_URL;
 
+declare global {
+  var mongoose: {
+    conn: Mongoose | null;
+  };
+}
+
+if (!DATABASE_URL) throw new Error("DATABASE_URL is not defined.");
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null };
+}
+
+export const connectMongoDB = async () => {
   try {
-    mongoose.connect(MONGODB_URL!, {
-      dbName: "next-js-cms-with-apollo-graphql",
-      bufferCommands: true,
-    });
-    console.log("Connected");
-  } catch (error: any) {
-    console.log(error);
+    if (cached.conn) {
+      console.log(pc.green("MongoDB Connected"));
+
+      return cached.conn;
+    }
+
+    cached.conn = await mongoose.connect(DATABASE_URL);
+    console.log("MongoDB Connection Created");
+    return cached.conn;
+  } catch (error) {
+    console.log("Something went wrong with MongoDB connection");
   }
 };
 
-export default connectDB;
+export default connectMongoDB;
