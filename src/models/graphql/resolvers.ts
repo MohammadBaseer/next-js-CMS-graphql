65bf15d6f782fbb5d5d754b449b7b4c848c5dcce
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-07-30 14:02:43 +0200

    Fetching Data From EndPoint Created (TypeDefs & Resolver Created)

--- src/models/graphql/resolvers.ts
+++ src/models/graphql/resolvers.ts
@@ -1,6 +1,38 @@
+import connectMongoDB from "@/lib/connectDB";
+import userModel from "../mongoose/userModel";
+import blogContextModel from "../mongoose/blogModel";
+import { GraphQLError } from "graphql";
+import videoBlogContentModel from "../mongoose/videoBlogModel";
+
 const resolvers = {
+  //! This os Query function to get the data from from MongooseDB
   Query: {
-    hello: () => "world",
+    async user() {
+      await connectMongoDB();
+      const documentCount = await userModel.countDocuments();
+      if (documentCount === 0) {
+        throw new GraphQLError("Not found");
+      }
+      console.log(documentCount);
+      return await userModel.find();
+    },
+    // !
+    async blogContext() {
+      const documentCount = await blogContextModel.countDocuments();
+      if (documentCount === 0) {
+        throw new GraphQLError("Not found");
+      }
+      return await blogContextModel.find();
+    },
+    // !
+    async videoBlogContext() {
+      const documentCount = await videoBlogContentModel.countDocuments();
+      if (documentCount === 0) {
+        throw new GraphQLError("Not found");
+      }
+      return await videoBlogContentModel.find();
+    },
+    // !
   },
 };
 
--- src/models/graphql/typeDefs.ts
+++ src/models/graphql/typeDefs.ts
@@ -1,8 +1,33 @@
 import gql from "graphql-tag";
 
 const typeDefs = gql`
+  # //! Types Decelerations
+  type Users {
+    id: ID!
+    name: String!
+    email: String!
+    password: String!
+    roll: String!
+    avatar: String!
+  }
+
+  type BlogContext {
+    id: ID!
+    title: String!
+    description: String!
+    photo: String!
+  }
+  type VideoBlogContext {
+    id: ID!
+    title: String!
+    url: String!
+  }
+
+  # //! Create Query to get the data from Postman/Apollo SandBox
   type Query {
-    hello: String
+    user: [Users!]!
+    blogContext: [BlogContext!]!
+    videoBlogContext: [VideoBlogContext!]!
   }
 `;
 
