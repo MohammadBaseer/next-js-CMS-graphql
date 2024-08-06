e4648bb6450eab740cc17ed64f90dfd2279a6d18
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-08-06 22:14:21 +0200

    small changes

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -5,12 +5,7 @@ export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
   return new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
-      // this needs to be an absolute url, as relative urls cannot be used in SSR
-      //   uri: "https://countries.trevorblades.com/",
-      uri: "http://localhost:3000/api/graphql",
-      // you can disable result caching here if you want to
-      // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
-      // fetchOptions: { cache: "no-store" },
+      uri: process.env.NEXT_PUBLIC_GRAPHQL_URI,
     }),
   });
 });
