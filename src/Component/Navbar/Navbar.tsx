5a2a3580b0e1804ae7ef3fb54e56e1e18b3b10f2
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-08-08 22:45:55 +0200

    Navbar Designed

--- src/Component/Navbar/Navbar.tsx
+++ src/Component/Navbar/Navbar.tsx
@@ -1,9 +1,63 @@
+"use client";
+import Link from "next/link";
 import styles from "./Navbar.module.scss";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [profileNavbarToggle, setProfileNavbarToggle] = useState<boolean>(false);
+
+  const toggle = () => {
+    if (profileNavbarToggle) {
+      setProfileNavbarToggle(false);
+    } else {
+      setProfileNavbarToggle(true);
+    }
+  };
+
   return (
     <div className={styles.navbar_main}>
-      <h1>Navbar</h1>
+      <div className={styles.nav_container}>
+        <div className={styles.box_1}>
+          <h3>Name</h3>
+        </div>
+
+        <div className={styles.box_2}>
+          <div className=""></div>
+
+          <div className={styles.profile_image_box}>
+            <img className={styles.profile_image} src="https://c8.alamy.com/compde/2j3b2t7/3d-illustration-eines-lachelnden-geschaftsmannes-nahaufnahme-portrat-cute-cartoon-mann-avatar-charakter-gesicht-isoliert-auf-weissem-hintergrund-2j3b2t7.jpg" alt="" onClick={toggle} />
+
+            <div className={styles.user_tab_navbar_element_box} style={profileNavbarToggle === true ? { display: "block" } : { display: "none" }}>
+              <div className={styles.user_tab_navbar_element}>
+                <img className={styles.user_photo} src="" alt="" onClick={toggle} />
+                
+              </div>
+
+              <div className={styles.user_tab_navbar_element}>
+                <Link className={styles.href} href="/">
+                  <span className="pi pi-user"> My Profile</span>
+                </Link>
+              </div>
+              {/* <div className={styles.user_tab_navbar_element}>
+                <Link className={styles.href} href="/dashboard">
+                  {" "}
+                  <span className="pi pi-shop"> Admin Panel</span>{" "}
+                </Link>
+              </div> */}
+              {/* <div className={styles.user_tab_navbar_element}>
+                      <Link className={styles.href} href="/register">
+                        <span className="pi pi-cog"> Settings</span>
+                      </Link>
+                    </div> */}
+              <div className={styles.user_tab_navbar_element}>
+                <Link className={styles.href} href="#">
+                  <span className="pi pi-sign-out"> Logout</span>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
