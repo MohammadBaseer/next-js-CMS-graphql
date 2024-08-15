ab3b684cbd95b5d4da9ad2dcecaa7082016c4cd1
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-08-15 23:51:40 +0200

    CMS is Ready :) "I Hope"

--- src/Component/Sidebar/Sidebar.tsx
+++ src/Component/Sidebar/Sidebar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import styles from "./Sidebar.module.scss";
 import "primeicons/primeicons.css";
 import withAuth from "../RoutesProtect/withAuth";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "@/context/authContext";
 import { useSuspenseQuery } from "@apollo/client";
 import { GetSingleUsersType } from "@/types/customTypes/UsersCustomTypes";
@@ -11,6 +11,7 @@ import { GET_USERS_BY_ID } from "@/queries/userQuery";
 
 const Sidebar = () => {
   const { userProfile } = useContext(AuthContext);
+  const [isActive, setIsActive] = useState(false);
 
   const { data: userData } = useSuspenseQuery<GetSingleUsersType>(GET_USERS_BY_ID, {
     variables: {
@@ -19,71 +20,82 @@ const Sidebar = () => {
   });
   const userRole = userData.user.role;
 
+  const handleToggle = () => {
+    setIsActive(!isActive);
+  };
+
   return (
-    <div className={styles.main_container}>
-      <div className={styles.navbar}>
-        <h1>CMS</h1>
-        <div className={styles.nav_elements}>
-          <ul className={styles.ul_main}>
-            <li className={styles.li_main}>
-              <span className="pi pi-home">&nbsp;Dashboard</span>
-            </li>
-            <ul className={styles.ul_sub}>
-              <li className={styles.li_sub}>
-                <Link className={styles.link} href="/home">
-                  <span className="pi pi-angle-right">&nbsp;Home</span>
-                </Link>
+    <>
+      <div className={`${styles.hamburger} ${isActive ? styles.toggle : ""}`} onClick={handleToggle} style={isActive ? { position: "absolute", left: "155px", top: "10px" } : { position: "absolute", left: "5px", top: "10px" }}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className={styles.main_container} style={isActive === true ? { display: "block" } : {}}>
+        <div className={styles.navbar}>
+          <h1 style={{ borderBottom: "solid" }}>CMS</h1>
+          <div className={styles.nav_elements}>
+            <ul className={styles.ul_main}>
+              <li className={styles.li_main}>
+                <span className="pi pi-home">&nbsp;Dashboard</span>
               </li>
-            </ul>
-            <li className={styles.li_main}>
-              <span className="pi pi-book">&nbsp;Context</span>
-            </li>
-            <ul className={styles.ul_sub}>
-              <li className={styles.li_sub}>
-                <Link className={styles.link} href="/blogcontext">
-                  <span className="pi pi-angle-right">&nbsp;Blog Context</span>
-                </Link>
-                <Link className={styles.link} href="/mediacontext">
-                  <span className="pi pi-angle-right">&nbsp;Media Context</span>
-                </Link>
+              <ul className={styles.ul_sub}>
+                <li className={styles.li_sub}>
+                  <Link className={styles.link} href="/home">
+                    <span className="pi pi-angle-right">&nbsp;Home</span>
+                  </Link>
+                </li>
+              </ul>
+              <li className={styles.li_main}>
+                <span className="pi pi-book">&nbsp;Context</span>
               </li>
+              <ul className={styles.ul_sub}>
+                <li className={styles.li_sub}>
+                  <Link className={styles.link} href="/blogcontext">
+                    <span className="pi pi-angle-right">&nbsp;Blog Context</span>
+                  </Link>
+                  <Link className={styles.link} href="/mediacontext">
+                    <span className="pi pi-angle-right">&nbsp;Media Context</span>
+                  </Link>
+                </li>
+              </ul>
+
+              {userRole === "Admin" ? (
+                <>
+                  <li className={styles.li_main}>
+                    <span className="pi pi-cog">&nbsp;Roles Config</span>
+                  </li>
+
+                  <ul className={styles.ul_sub}>
+                    <li className={styles.li_sub}>
+                      <Link className={styles.link} href="/role_Config">
+                        <span className="pi pi-angle-right">&nbsp;Roles</span>
+                      </Link>
+                    </li>
+                  </ul>
+
+                  <li className={styles.li_main}>
+                    <span className="pi">
+                      <i className="pi pi-spin pi-cog"></i>&nbsp;Settings
+                    </span>
+                  </li>
+
+                  <ul className={styles.ul_sub}>
+                    <li className={styles.li_sub}>
+                      <Link className={styles.link} href="/users">
+                        <span className="pi pi-angle-right">&nbsp;Users</span>
+                      </Link>
+                    </li>
+                  </ul>
+                </>
+              ) : (
+                ""
+              )}
             </ul>
-
-            {userRole === "Admin" ? (
-              <>
-                <li className={styles.li_main}>
-                  <span className="pi pi-cog">&nbsp;Roles Config</span>
-                </li>
-
-                <ul className={styles.ul_sub}>
-                  <li className={styles.li_sub}>
-                    <Link className={styles.link} href="/role_Config">
-                      <span className="pi pi-angle-right">&nbsp;Roles</span>
-                    </Link>
-                  </li>
-                </ul>
-
-                <li className={styles.li_main}>
-                  <span className="pi">
-                    <i className="pi pi-spin pi-cog"></i>&nbsp;Settings
-                  </span>
-                </li>
-
-                <ul className={styles.ul_sub}>
-                  <li className={styles.li_sub}>
-                    <Link className={styles.link} href="/users">
-                      <span className="pi pi-angle-right">&nbsp;Users</span>
-                    </Link>
-                  </li>
-                </ul>
-              </>
-            ) : (
-              ""
-            )}
-          </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
