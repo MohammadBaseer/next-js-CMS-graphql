dfac3c62d5a602f2f0415653d2ec36bf587ffbd4
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-08-16 13:40:38 +0200

    Home Modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,26 @@ export default function Home() {
       <WebNavbar />
       <div>
         <h1>Welcome To The GraphQL 'CMS' Project</h1>
-        <h3></h3>
+        <h3>User Base Roles: </h3>
+        <br />
+        <h4> Admin: Full Access</h4>
+        ---{">"} - Add Post Blogs ---{">"} Update Post Blogs ---{">"} Delete Post Blogs
+        <br /> ---{">"} - Add Video Blogs ---{">"} Update Video Blogs ---{">"} Delete Video Blogs
+        <br /> ---{">"} - Add User ---{">"} Change User Roles
+        <br /> ---{">"} - Profile Update
+        <br />
+        <br />
+        <h4> Editor: Only Edit the Data</h4>
+        ---{">"} - Add Post Blogs ---{">"} Update Post Blogs ---{">"} only Delete Own Post Blogs
+        <br /> ---{">"} - Add Video Blogs ---{">"} Update Video Blogs ---{">"} only Delete Own Video Blogs
+        <br /> ---{">"} - Profile Update
+        <br />
+        <br />
+        <h4> Other Users: Access to own Data</h4>
+        ---{">"} - Add Own Post Blogs ---{">"} Update Own Post Blogs ---{">"} Delete Own Post Blogs
+        <br /> ---{">"} - Add Own Video Blogs ---{">"} Update Own Video Blogs ---{">"} Delete Own Video Blogs
+        <br /> ---{">"} - Profile Update
+        <br />
       </div>{" "}
     </>
   );
