71c4e36b5075fe0ba45c82e1a36eb99d0f263e25
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-08-17 14:07:10 +0200

    modified

--- src/app/(Website)/video_blogs/page.tsx
+++ src/app/(Website)/video_blogs/page.tsx
@@ -6,7 +6,6 @@ import { useSuspenseQuery } from "@apollo/client";
 import { GetAllVideoBlogsTypes } from "@/types/customTypes/VideoCustomTypes";
 import { VIDEO_BLOG_CONTEXT } from "@/queries/videoBlogContextQuery";
 import getYouTubeID from "get-youtube-id";
-import { useEffect } from "react";
 
 export default function VideoBlogs() {
   const { data, error } = useSuspenseQuery<GetAllVideoBlogsTypes>(VIDEO_BLOG_CONTEXT);
@@ -19,9 +18,9 @@ export default function VideoBlogs() {
     return <p>Loading...</p>;
   }
 
-  useEffect(() => {
-    document.title = "Video Blogs";
-  }, []);
+  // useLayoutEffect(() => {
+  //   document.title = "Video Blogs";
+  // }, []);
 
   return (
     <section className={styles.videoBlogs}>
