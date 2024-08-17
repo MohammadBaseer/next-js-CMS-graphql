262b78a886284274e2cc75e180566022d237253e
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-08-17 15:37:23 +0200

    Test page for Deployment Mode

--- src/app/(Website)/blogs/[id]/page.tsx
+++ src/app/(Website)/blogs/[id]/page.tsx
@@ -1,60 +1,44 @@
 "use client";
 
-import Link from "next/link";
-import styles from "./BlogDetail.module.scss"; // Adjust the path as necessary
-import { useSuspenseQuery } from "@apollo/client";
-import { GetSingleBlogContextType } from "@/types/customTypes/PostBlogCustomTypes";
-import { GET_POST_CONTEXT_BY_ID } from "@/queries/postContextQuery";
-import { useEffect } from "react";
-import { useParams } from "next/navigation";
+// import Link from "next/link";
+// import styles from "./BlogDetail.module.scss"; // Adjust the path as necessary
+// import { useSuspenseQuery } from "@apollo/client";
+// import { GetSingleBlogContextType } from "@/types/customTypes/PostBlogCustomTypes";
+// import { GET_POST_CONTEXT_BY_ID } from "@/queries/postContextQuery";
+// import { useEffect } from "react";
 
-type ParamsPropTypes = {
-  searchParams: {
-    view: string;
-  };
-};
+// type ParamsPropTypes = {
+//   params: {
+//     id: string;
+//   };
+// };
+
+// export default function BlogDetail({ params: { id } }: ParamsPropTypes) {
+//   console.log(":::::::::::===>", id);
+
+//   const { data: fetchData } = useSuspenseQuery<GetSingleBlogContextType>(GET_POST_CONTEXT_BY_ID, {
+//     variables: {
+//       blogContextId: id,
+//     },
+//   });
+
+//   useEffect(() => {
+//     document.title = "Post Blog Add";
+//   }, []);
+
+//   return (
+//     <div className={styles.blogDetail}>
+//       <Link href="/blogs" className={styles.backLink}>
+//         ← Back to Blogs
+//       </Link>
+//       <h1 className={styles.title}>{fetchData.blogContext.title}</h1>
+//       <img src={fetchData.blogContext.photo.url} alt={fetchData.blogContext.title} className={styles.blogImage} />
+//       <p className={styles.content}>{fetchData.blogContext.description}</p>
+
+//     </div>
+//   );
+// }
 
 export default function BlogDetail() {
-  // export default function BlogDetail({ searchParams: { view } }: ParamsPropTypes) {
-
-  const params = useParams<{ id: string }>();
-  const { id } = params;
-
-  const { data: fetchData } = useSuspenseQuery<GetSingleBlogContextType>(GET_POST_CONTEXT_BY_ID, {
-    variables: {
-      blogContextId: id,
-    },
-  });
-  useEffect(() => {
-    document.title = "Post Blog Add";
-  }, []);
-
-  return (
-    <div className={styles.blogDetail}>
-      <Link href="/blogs" className={styles.backLink}>
-        ← Back to Blogs
-      </Link>
-      <h1 className={styles.title}>{fetchData.blogContext.title}</h1>
-      <img src={fetchData.blogContext.photo.url} alt={fetchData.blogContext.title} className={styles.blogImage} />
-      <p className={styles.content}>{fetchData.blogContext.description}</p>
-
-      {/* <div className={styles.commentsSection}>
-        <h2 className={styles.commentsTitle}>Comments</h2>
-        <ul className={styles.commentsList}>
-          {comments.map((comment) => (
-            <li key={comment.id} className={styles.comment}>
-              <strong>{comment.name}:</strong> {comment.content}
-            </li>
-          ))}
-        </ul>
-
-        <div className={styles.commentForm}>
-          <textarea className={styles.commentInput} value={newComment} onChange={(e) => setNewComment(e.target.value)} placeholder="Add a comment..."></textarea>
-          <button className={styles.commentButton} onClick={handleAddComment}>
-            Post Comment
-          </button>
-        </div>
-      </div> */}
-    </div>
-  );
+  return <h1>This is the test Page</h1>;
 }
